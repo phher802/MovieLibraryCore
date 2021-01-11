12be9ec0a5fa7106135c4d92ed0a4e1fe72963ce
phher802 <your_email@example.com>
2021-01-11 15:21:32 -0600

    Create get post method in app js

--- Client/app.js
+++ Client/app.js
@@ -1,27 +1,44 @@
-(function($){
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value,
-            Director: this["director"].value,
-            Genre: this["genre"].value
-        };
-
-        function getAllMovies(){
-            $.ajax({
-                url: 'https://localhost:44325/api/movie',
-                dataType: 'json',
-                type: 'get',
-                contentType: 'application/json',
-                data: JSON.stringify(dict),
-                success: function(data, textStatus, jQxhr){
-    
-                },
-                error: function(jqxhr, textStatus, errorThrown){
-                    console.log(errorThrown);
-                }
-            })
+(function ($) {
+        function processForm(e) {
+            var dict = {
+                Title: this["title"].value,
+                Director: this["director"].value,
+                Genre: this["genre"].value
+            };        
         }
-        
+
+        function getAllMovies() {
+            // $("button").on('click', function(e){
+            //     e.preventDefault();
+            //     console.log("successfull click");
+            // })
+            $(document).ready(function () {
+                $.ajax({
+                        url: 'https://localhost:44325/api/movie',
+                        dataType: 'json',
+                        type: 'GET',
+                        contentType: 'application/json',
+                        data: JSON.stringify(dict),
+                        success: function () {
+                            $('.movieData').html('');
+                        }
+                    })
+                    .then(function (data) {
+                        $.each(data, function (index, value) {
+                            $('.movieData').append()
+                            "<tr>" +
+                            "<td>" + value.movieId + "</td>"
+                            "<td>" + value.Title + "</td>"
+                            "<td>" + value.Director + "</td>"
+                            "<td>" + value.Genre + "</td>"
+                            "</tr>"
+                        });
+
+                    });
+                });
+
+            };
+        });
 
         $.ajax({
             url: 'https://localhost:44325/api/movie',
@@ -29,16 +46,16 @@
             type: 'post',
             contentType: 'application/json',
             data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
+            success: function (data, textStatus, jQxhr) {
+                $('#response pre').html(data);
             },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
             }
         });
 
         e.preventDefault();
-    }
+    
 
-    $('#my-form').submit( processForm );
+    $('#my-form').submit(processForm);
 })(jQuery);
