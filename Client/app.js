29ad9b8408c3946a00216c75e10426777989bd20
GRy82 <gwryan@buffalo.edu>
2021-01-11 19:53:43 -0500

    jquery put functionallity written out in app.js

--- Client/app.js
+++ Client/app.js
@@ -27,15 +27,43 @@
 
 })(jQuery); 
 
+$("#edit-button").on("click", 
+    function processForm( e ){
+        var dict = {
+            Title : this["title"].value,
+            Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: 'json',
+        contentType: 'application/json',
+        type: 'put',
+        data: JSON.stringify(dict),
+        success: function( data, textStatus, jQxhr ){
+            $('#response pre').html( '' );
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    })
+
+    $('#edit-form').submit( data );
+    }
+)(jQuery); 
+
+
 $(document).ready($.get('https://localhost:44325/api/movie', function(data){
        data.map(element => {
-            $('#movie-table').append(`<tr><td id="${element.MovieId}">${element.Title}</td>
-                <td>${element.Director}</td>
-                <td>${element.Genre}</td></tr>`
+            $('#movie-table').append(`<tr><td id="${element.movieId}">${element.title}</td>
+                <td>${element.director}</td>
+                <td>${element.genre}</td></tr>`
             );
         });
     })
 )
+
 
 
 
