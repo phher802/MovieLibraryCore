573f9d89b367fd40cae21cd0b818912fcd3d60b5
phher802 <your_email@example.com>
2021-01-12 08:56:10 -0600

    Edit get function in app js file

--- Client/app.js
+++ Client/app.js
@@ -38,57 +38,61 @@ $(document).ready($.get('https://localhost:44325/api/movie', function (data) {
     });
 }))
 
-    $.ajax({
-        url: 'https://localhost:44325/api/movie',
-        dataType: 'json',
-        contentType: 'application/json',
-        type: 'put',
-        data: JSON.stringify(dict),
-        success: function( data, textStatus, jQxhr ){
-            $('#response pre').html( '' );
-        },
-        error: function( jqXhr, textStatus, errorThrown ){
-            console.log( errorThrown );
-        }
-    })
-
-    $('#edit-form').submit( data );
+$.ajax({
+    url: 'https://localhost:44325/api/movie',
+    dataType: 'json',
+    contentType: 'application/json',
+    type: 'put',
+    data: JSON.stringify(dict),
+    success: function (data, textStatus, jQxhr) {
+        $('#response pre').html('');
+    },
+    error: function (jqXhr, textStatus, errorThrown) {
+        console.log(errorThrown);
     }
-); 
 
-$(".btn").on("click", function getMovie(id){
+    $('#edit-form').submit(data);
+})
+
+
+$(".btn").on("click", function getMovie(movie) {
     console.log("button clicked");
-    $(document).ready(function(){
+    $(document).ready(function () {
         $.ajax({
             type: 'GET',
-            url: 'https://localhost:44325/api/movie' + movieId,
+            url: 'https://localhost:44325/api/movie' + movie,
             dataType: 'json',
             contentType: 'application/json',
-            data: JSON.stringify({"scale": {"desired-instances": 123}})
-        }).then(function(data){
-           // data.movieId.find();
-          //  var foundMovie = document.getElementById('movieId').value;
+            data: JSON.stringify({
+                "scale": {
+                    "desired-instances": 123
+                }
+            })
+        }).then(function (data) {
+            // data.movieId.find();
+            //  var foundMovie = document.getElementById('movieId').value;
             console.log(data);
         });
     });
 });
 
-$("button").on("click", function getMovie(){
-    console.log("button clicked");
-    $(document).ready($.get('https://localhost:44325/api/movie' + id, function (data){
-        data.contains(element => $('#movie-table'))
-    }))
-});
-function sendId(id){
-    $.get('https://localhost:44325/api/movie/' +id, function(data){
+
+// $("button").on("click", function getMovie(id){
+//     console.log("button clicked");
+//     $(document).ready($.get('https://localhost:44325/api/movie' + id, function (data){
+//         data.contains(element => $('#movie-table'))
+//     }))
+// });
+
+
+function sendId(id) {
+    $.get('https://localhost:44325/api/movie/' + id, function (data) {
         $(".edit-section").css("display", "inline")
         data.map(element => {
             $('#movie-details').append(`<dt>Title</dt><dd>${element.title}</dd>
                 <dt>Director</dt><dd>${element.director}</dd>
-                <dt>Genre</dt><dd>${element.genre}</dd>`
-            );
+                <dt>Genre</dt><dd>${element.genre}</dd>`);
         });
 
-    })  
+    })
 }
-
